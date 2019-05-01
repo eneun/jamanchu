b74ee1996d6b03d97452c9636e1e3d3f81977b15
eneun <wtcat@naver.com>
2019-05-02 00:42:45 +0900

    meeting model edit

--- meeting/views.py
+++ meeting/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
 from .models import Meeting
 from .forms import MeetingForm
 
@@ -15,34 +15,7 @@ def detail(request, meeting_id):
 def new(request):
     return render(request, 'meeting/new.html')
 
-# def create(request):
-#     meeting = Meeting()
-#     meeting.title = request.GET['title']
-#     meeting.body = request.GET['body']
-#     meeting.date = request.GET['date']
-#     meeting.start_time = request.GET['start_time']
-#     meeting.end_time = request.GET['end_time']
-#     meeting.save()
-#     return redirect('/meeting/'+str(meeting.id))
-
-
-# 일반폼 사용시
-# def meeting_new(request):
-#     if request.method == 'POST':
-#         form = MeetingPost(request.POST)
-
-#         if form.is_valid():
-#             meeting = Meeting()
-#             meeting.category = form.cleaned_data['category']
-#             meeting.title = form.cleaned_data['title']
-#             meeting.body = form.cleaned_data['body']
-#             meeting.save()
-#             return redirect('listpage')
-#     else:
-#         form = MeetingPost()
-#     return render(request, 'meeting/new.html', {'form': form})
-
-# 모델폼 사용시
+# 모델폼 사용
 def meeting_new(request):
     if request.method == 'POST':
         form = MeetingForm(request.POST)
@@ -51,6 +24,8 @@ def meeting_new(request):
             # post.category = 'hi'
             post.save()
             return redirect('listpage')
+        # else:
+        #     return HttpResponseRedirect('/')
     else:
         form = MeetingForm()
         return render(request, 'meeting/new.html', {'form': form})
